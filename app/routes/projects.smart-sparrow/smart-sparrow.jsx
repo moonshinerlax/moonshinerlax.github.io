290cdba30ab9aaeee6e4c05add9485d7e3f46889
Ivan Lap <aivenpromedia@gmail.com>
2024-03-09 01:45:20 -0600

    first commit

---
+++ app/routes/projects.smart-sparrow/smart-sparrow.jsx
@@ -0,0 +1,229 @@
+import backgroundSprLarge from '~/assets/spr-background - large.jpg';
+import backgroundSprPlaceholder from '~/assets/spr-background-placeholder.jpg';
+import imageSprBackgroundVolcanismLarge from '~/assets/spr-background-volcanism-large.jpg';
+import imageSprBackgroundVolcanismPlaceholder from '~/assets/spr-background-volcanism-placeholder.jpg';
+import imageSprBackgroundVolcanism from '~/assets/spr-background-volcanism.jpg';
+import backgroundSpr from '~/assets/spr-background.jpg';
+import imageSprComponentsDarkLarge from '~/assets/spr-components-dark-large.png';
+import imageSprComponentsDarkPlaceholder from '~/assets/spr-components-dark-placeholder.png';
+import imageSprComponentsDark from '~/assets/spr-components-dark.png';
+import imageSprComponentsLightLarge from '~/assets/spr-components-light-large.png';
+import imageSprComponentsLightPlaceholder from '~/assets/spr-components-light-placeholder.png';
+import imageSprComponentsLight from '~/assets/spr-components-light.png';
+import imageSprDesignSystemDarkLarge from '~/assets/spr-design-system-dark-large.png';
+import imageSprDesignSystemDarkPlaceholder from '~/assets/spr-design-system-dark-placeholder.png';
+import imageSprDesignSystemDark from '~/assets/spr-design-system-dark.png';
+import imageSprDesignSystemLightLarge from '~/assets/spr-design-system-light-large.png';
+import imageSprDesignSystemLightPlaceholder from '~/assets/spr-design-system-light-placeholder.png';
+import imageSprDesignSystemLight from '~/assets/spr-design-system-light.png';
+import imageSprLessonBuilderDarkLarge from '~/assets/spr-lesson-builder-dark-large.jpg';
+import imageSprLessonBuilderDarkPlaceholder from '~/assets/spr-lesson-builder-dark-placeholder.jpg';
+import imageSprLessonBuilderDark from '~/assets/spr-lesson-builder-dark.jpg';
+import imageSprLessonBuilderLightLarge from '~/assets/spr-lesson-builder-light-large.jpg';
+import imageSprLessonBuilderLightPlaceholder from '~/assets/spr-lesson-builder-light-placeholder.jpg';
+import imageSprLessonBuilderLight from '~/assets/spr-lesson-builder-light.jpg';
+import videoSprMotionLarge from '~/assets/spr-motion-large.mp4';
+import videoSprMotionPlaceholder from '~/assets/spr-motion-placeholder.jpg';
+import videoSprMotion from '~/assets/spr-motion.mp4';
+import { Image } from '~/components/image';
+import { SegmentedControl, SegmentedControlOption } from '~/components/segmented-control';
+import { ThemeProvider, useTheme } from '~/components/theme-provider';
+import {
+  ProjectBackground,
+  ProjectContainer,
+  ProjectHeader,
+  ProjectImage,
+  ProjectSection,
+  ProjectSectionColumns,
+  ProjectSectionContent,
+  ProjectSectionHeading,
+  ProjectSectionText,
+  ProjectTextRow,
+} from '~/layouts/project';
+import { baseMeta } from '~/utils/meta';
+import { media } from '~/utils/style';
+import styles from './smart-sparrow.module.css';
+
+const title = 'Administration Portal';
+const description =
+  'I created a web portal for a small business, in which they were able to create clients and employee users.';
+const roles = [
+  'Creator',
+  'UX and UI Design',
+  'Front End Development',
+  'Back End Development',
+  'Database Constructor',
+];
+
+export const meta = () => {
+  return baseMeta({ title, description, prefix: 'Projects' });
+};
+
+export const SmartSparrow = () => {
+  const { theme, toggleTheme } = useTheme();
+  const isDark = theme === 'dark';
+  const themes = ['dark', 'light'];
+
+  const handleThemeChange = index => {
+    toggleTheme(themes[index]);
+  };
+
+  return (
+    <>
+      <ProjectContainer>
+        <ProjectBackground
+          opacity={isDark ? 0.5 : 0.8}
+          src={backgroundSpr}
+          srcSet={`${backgroundSpr} 1080w, ${backgroundSprLarge} 2160w`}
+          placeholder={backgroundSprPlaceholder}
+        />
+        <ProjectHeader
+          title={title}
+          description={description}
+          url="https://esteticaziranda.vercel.app/"
+          roles={roles}
+        />
+        <ProjectSection padding="top">
+          <ProjectSectionContent>
+            <ProjectImage
+              raised
+              key={theme}
+              srcSet={
+                isDark
+                  ? `${imageSprLessonBuilderDark} 1280w, ${imageSprLessonBuilderDarkLarge} 2560w`
+                  : `${imageSprLessonBuilderLight} 1280w, ${imageSprLessonBuilderLightLarge} 2560w`
+              }
+              width={1280}
+              height={800}
+              placeholder={
+                isDark
+                  ? imageSprLessonBuilderDarkPlaceholder
+                  : imageSprLessonBuilderLightPlaceholder
+              }
+              sizes={`(max-width: ${media.mobile}px) 100vw, (max-width: ${media.tablet}px) 800px, 1000px`}
+              alt=""
+            />
+          </ProjectSectionContent>
+        </ProjectSection>
+        <ProjectSection>
+          <ProjectTextRow>
+            <ProjectSectionHeading>The problem</ProjectSectionHeading>
+            <ProjectSectionText>
+              Estetica Ziranda started as a small saloon owned by a young lady, after 9 years on the business,
+              she decides to take the next big step and make it official. Gaining a lot of local popularity thanks
+              to her social media posts, the project got bigger and they did not had any tools to control and organized
+              all the administration, sales and expenses.
+            </ProjectSectionText>
+          </ProjectTextRow>
+        </ProjectSection>
+        <ProjectSection light={isDark}>
+          <ProjectSectionContent>
+            <Image
+              key={theme}
+              srcSet={
+                isDark
+                  ? `${imageSprComponentsDark} 1024w, ${imageSprComponentsDarkLarge} 2048w`
+                  : `${imageSprComponentsLight} 1024w, ${imageSprComponentsLightLarge} 2048w`
+              }
+              width={1024}
+              hright={800}
+              placeholder={
+                isDark
+                  ? imageSprComponentsDarkPlaceholder
+                  : imageSprComponentsLightPlaceholder
+              }
+              alt={`A set of ${theme} themed components for the aero design system`}
+              sizes="100vw"
+            />
+            <ProjectTextRow>
+              <SegmentedControl
+                currentIndex={themes.indexOf(theme)}
+                onChange={handleThemeChange}
+              >
+                <SegmentedControlOption>Dark theme</SegmentedControlOption>
+                <SegmentedControlOption>Light theme</SegmentedControlOption>
+              </SegmentedControl>
+            </ProjectTextRow>
+            <ProjectTextRow>
+              <ProjectSectionHeading>The control system</ProjectSectionHeading>
+              <ProjectSectionText>
+                To streamline the administration of clients and sales by each customer. 
+                I created a dashboard to view all customers, in which the user can edit and add new customer.
+                When clicking on a specific customer, the user will be able to see details such as Name, Birthdate, Phone Number, Email,
+                and Sales History.
+              </ProjectSectionText>
+            </ProjectTextRow>
+          </ProjectSectionContent>
+        </ProjectSection>
+        <ProjectSection>
+          <ProjectSectionContent>
+            <Image
+              raised
+              key={theme}
+              srcSet={
+                isDark
+                  ? `${imageSprDesignSystemDark} 1280w, ${imageSprDesignSystemDarkLarge} 2560w`
+                  : `${imageSprDesignSystemLight} 1280w, ${imageSprDesignSystemLightLarge} 2560w`
+              }
+              width={1280}
+              height={800}
+              placeholder={
+                isDark
+                  ? imageSprDesignSystemDarkPlaceholder
+                  : imageSprDesignSystemLightPlaceholder
+              }
+              alt="The Admin Dashboard 🔐 with User Role control"
+              sizes="100vw"
+            /> 
+            <ProjectTextRow>
+              <ProjectSectionHeading>The Admin Dashboard 🔐 with User Role control</ProjectSectionHeading>
+              <ProjectSectionText>
+                A admin portal is useless with out user roles and privileges.
+                All user with Admin Role  have access to the Admin Dashboard in which they are able to activate and deactivate user,
+                Switch their role and have also access to summary of sales and expenses, they can also download reports with such information.
+              </ProjectSectionText>
+            </ProjectTextRow>
+          </ProjectSectionContent>
+        </ProjectSection>
+        <ThemeProvider theme="dark" data-invert>
+          <ProjectSection
+            backgroundOverlayOpacity={0.5}
+            backgroundElement={
+              <Image
+                srcSet={`${imageSprBackgroundVolcanism} 1280w, ${imageSprBackgroundVolcanismLarge} 2560w`}
+                width={1280}
+                height={900}
+                placeholder={imageSprBackgroundVolcanismPlaceholder}
+                alt="A dramatic ocean scene with lava forming a new land mass."
+                sizes="100vw"
+              />
+            }
+          >
+            <ProjectSectionColumns width="full">
+              <ProjectSectionContent width="full">
+                <ProjectTextRow width="s">
+                  <ProjectSectionHeading>Interactive design</ProjectSectionHeading>
+                  <ProjectSectionText>
+                    The application had to be very user friendly as the users do not have deep, tech knowledge.
+                    Time had to be save by avoiding them to spend much time registering and viewing customers.
+                    This is why the interface is simple and easy to read.
+                  </ProjectSectionText>
+                </ProjectTextRow>
+              </ProjectSectionContent>
+              <Image
+                raised
+                className={styles.video}
+                srcSet={`${videoSprMotion} 1280w, ${videoSprMotionLarge} 2560w`}
+                width={1280}
+                height={800}
+                placeholder={videoSprMotionPlaceholder}
+                alt="A learning designer building and deploying an interactive lesson on volcanism using the app."
+                sizes={`(max-width: ${media.mobile}px) 100vw, 50vw`}
+              />
+            </ProjectSectionColumns>
+          </ProjectSection>
+        </ThemeProvider>
+        </ProjectContainer>
+    </>
+  );
+};
